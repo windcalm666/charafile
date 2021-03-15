9a59314fa1b57d0a74b92a30911fa903794b9bb9
windcalm666 <79242740+windcalm666@users.noreply.github.com>
2021-03-15 20:31:11 +0900

    ユーザーモデルにバリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :password
+  end
+
 end
