f6019ec610fad5ced043e34d79b24f4fb3b1a947
windcalm666 <79242740+windcalm666@users.noreply.github.com>
2021-04-07 19:54:44 +0900

    Shinobi結合テストコード実装

---
+++ spec/system/shinobis_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe "Shinobis", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+    @shinobi = FactoryBot.build(:shinobi)
+  end
+  context "キャラクター保存できる時" do
+    it "名前とセレクトボックスを入力していれば保存でき、トップページに遷移する" do
+      sign_in(@user)
+      find("#system-select").find("option[value='shinobigami']").select_option
+      expect(page).to have_content("新規キャラクター作成")
+      visit new_shinobi_path
+      fill_in "shinobi_name", with: @shinobi.name
+      find("#shinobi-school-select").find("option[value='3']").select_option
+      expect{find("input[name='commit']").click}.to change{ Shinobi.count }.by(1)
+      expect(current_path).to eq(root_path)
+    end
+  end
+
+  context "キャラクター保存できない時" do
+    it "名前とセレクトボックスが正しくないと新規作成ページのまま" do
+      sign_in(@user)
+      find("#system-select").find("option[value='shinobigami']").select_option
+      expect(page).to have_content("新規キャラクター作成")
+      visit new_shinobi_path
+      fill_in "shinobi_name", with: ""
+      find("#shinobi-school-select").find("option[value='0']").select_option
+      expect{find("input[name='commit']").click}.to change{ Shinobi.count }.by(0)
+      expect(current_path).to eq(shinobis_path)
+    end
+    end
+  end
+
+
+end
